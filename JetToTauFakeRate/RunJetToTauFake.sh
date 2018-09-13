a1b459444fdf2bc8a8560951384995636c30ec76
Andrew Loeliger <aloelige@cern.ch>
2018-09-13 15:02:29 +0200

    Added in support for the new code that gets rid of DY events in the jets shape.

--- JetToTauFakeRate/RunJetToTauFake.sh
+++ JetToTauFakeRate/RunJetToTauFake.sh
@@ -9,6 +9,25 @@
 
 root -b -l -q JetToTauFakesMeasurement.cc
 root -b -l -q GenerateJetSamples.cc
+
+#new thing to handle the potential non jets that ends up in 
+#the jet region
+
+root -b -l -q GenerateMCInJetsRegion.cc\(\"DY\"\)
+root -b -l -q GenerateMCInJetsRegion.cc\(\"DY1\"\)
+root -b -l -q GenerateMCInJetsRegion.cc\(\"DY2\"\)
+root -b -l -q GenerateMCInJetsRegion.cc\(\"DY3\"\)
+root -b -l -q GenerateMCInJetsRegion.cc\(\"DY4\"\)
+root -b -l -q GenerateMCInJetsRegion.cc\(\"TTTo2L2Nu\"\)
+root -b -l -q GenerateMCInJetsRegion.cc\(\"TTToHadronic\"\)
+root -b -l -q GenerateMCInJetsRegion.cc\(\"TTToSemiLeptonic\"\)
+root -b -l -q GenerateMCInJetsRegion.cc\(\"WW\"\)
+root -b -l -q GenerateMCInJetsRegion.cc\(\"WZ\"\)
+root -b -l -q GenerateMCInJetsRegion.cc\(\"ZZ\"\)
+hadd -f ../Distributions/MCInJetsRegion.root ../TemporaryFiles/*InJetsRegion.root
+
+root -b -l -q RemoveMCFromJets.cc\(\"$1\"\)
+
 #cd ../TemporaryCode/
 #root -l CompareFakeRateDistributionsToMC.cc
 #cd ../JetToTauFakeRate/

71479cfb91203e7cbbe9d09e8234d3a9df4f897b
Andrew Loeliger <aloelige@cern.ch>
2018-11-30 12:38:27 +0100

    More plots for correct uncertainty model

--- JetToTauFakeRate/PrepareFakeRateForCombine.cc
+++ JetToTauFakeRate/PrepareFakeRateForCombine.cc
@@ -32,12 +32,50 @@ void PrepareFakeRateForCombine(string IsoWorkingPoint)
   TH1F* WW_Pass = (TH1F*) passDirectory->Get("WW_Pass");
   TH1F* WZ_Pass = (TH1F*) passDirectory->Get("WZ_Pass");
   TH1F* ZZ_Pass = (TH1F*) passDirectory->Get("ZZ_Pass");
+
+  //get the TES uncertainty for the Real Tau Distributions
   
+  TH1F* ZTauTau_TES_UP_Pass = (TH1F*) passDirectory->Get("GenMatch_DY_1.030000_Pass");
+  TH1F* ZTauTau1_TES_UP_Pass = (TH1F*) passDirectory->Get("GenMatch_DY1_1.030000_Pass");
+  TH1F* ZTauTau2_TES_UP_Pass = (TH1F*) passDirectory->Get("GenMatch_DY2_1.030000_Pass");
+  TH1F* ZTauTau3_TES_UP_Pass = (TH1F*) passDirectory->Get("GenMatch_DY3_1.030000_Pass");
+  TH1F* ZTauTau4_TES_UP_Pass = (TH1F*) passDirectory->Get("GenMatch_DY4_1.030000_Pass");
+  TH1F* ZTauTau_TES_DOWN_Pass = (TH1F*) passDirectory->Get("GenMatch_DY_0.970000_Pass");
+  TH1F* ZTauTau1_TES_DOWN_Pass = (TH1F*) passDirectory->Get("GenMatch_DY1_0.970000_Pass");
+  TH1F* ZTauTau2_TES_DOWN_Pass = (TH1F*) passDirectory->Get("GenMatch_DY2_0.970000_Pass");
+  TH1F* ZTauTau3_TES_DOWN_Pass = (TH1F*) passDirectory->Get("GenMatch_DY3_0.970000_Pass");
+  TH1F* ZTauTau4_TES_DOWN_Pass = (TH1F*) passDirectory->Get("GenMatch_DY4_0.970000_Pass");
+
+  //mu to tau fake shape uncertainty
+  TH1F* LowGenMatch_DY_Shape_UP_Pass = (TH1F*) passDirectory->Get("LowGenMatch_DY_1.100000_Pass");
+  TH1F* LowGenMatch_DY1_Shape_UP_Pass = (TH1F*) passDirectory->Get("LowGenMatch_DY1_1.100000_Pass");
+  TH1F* LowGenMatch_DY2_Shape_UP_Pass = (TH1F*) passDirectory->Get("LowGenMatch_DY2_1.100000_Pass");
+  TH1F* LowGenMatch_DY3_Shape_UP_Pass = (TH1F*) passDirectory->Get("LowGenMatch_DY3_1.100000_Pass");
+  TH1F* LowGenMatch_DY4_Shape_UP_Pass = (TH1F*) passDirectory->Get("LowGenMatch_DY4_1.100000_Pass");
+  TH1F* LowGenMatch_DY_Shape_DOWN_Pass = (TH1F*) passDirectory->Get("LowGenMatch_DY_0.900000_Pass");
+  TH1F* LowGenMatch_DY1_Shape_DOWN_Pass = (TH1F*) passDirectory->Get("LowGenMatch_DY1_0.900000_Pass");
+  TH1F* LowGenMatch_DY2_Shape_DOWN_Pass = (TH1F*) passDirectory->Get("LowGenMatch_DY2_0.900000_Pass");
+  TH1F* LowGenMatch_DY3_Shape_DOWN_Pass = (TH1F*) passDirectory->Get("LowGenMatch_DY3_0.900000_Pass");
+  TH1F* LowGenMatch_DY4_Shape_DOWN_Pass = (TH1F*) passDirectory->Get("LowGenMatch_DY4_0.900000_Pass");
+
+  LowGenMatch_DY_Shape_UP_Pass->Add(LowGenMatch_DY1_Shape_UP_Pass);
+  LowGenMatch_DY_Shape_UP_Pass->Add(LowGenMatch_DY2_Shape_UP_Pass);
+  LowGenMatch_DY_Shape_UP_Pass->Add(LowGenMatch_DY3_Shape_UP_Pass);
+  LowGenMatch_DY_Shape_UP_Pass->Add(LowGenMatch_DY4_Shape_UP_Pass);
+  LowGenMatch_DY_Shape_DOWN_Pass->Add(LowGenMatch_DY1_Shape_DOWN_Pass);
+  LowGenMatch_DY_Shape_DOWN_Pass->Add(LowGenMatch_DY2_Shape_DOWN_Pass);
+  LowGenMatch_DY_Shape_DOWN_Pass->Add(LowGenMatch_DY3_Shape_DOWN_Pass);
+  LowGenMatch_DY_Shape_DOWN_Pass->Add(LowGenMatch_DY4_Shape_DOWN_Pass);
+
   //Get the fake rates determined stuff
   TFile* FakeRateDeterminedDistributions = new TFile("../Distributions/FakeRateDeterminedDistributions.root");
   TH1F* JetDistribution; 
-  TH1F* JetDistribution_DOWN;
-  TH1F* JetDistribution_UP;
+  TH1F* JetDistribution_DecayMode0_DOWN;
+  TH1F* JetDistribution_DecayMode0_UP;
+  TH1F* JetDistribution_DecayMode1_DOWN;
+  TH1F* JetDistribution_DecayMode1_UP;
+  TH1F* JetDistribution_DecayMode10_DOWN;
+  TH1F* JetDistribution_DecayMode10_UP;
   assert(IsoWorkingPoint == "VLoose" ||
 	 IsoWorkingPoint == "Loose" ||
 	 IsoWorkingPoint == "Medium" ||
@@ -46,42 +84,64 @@ void PrepareFakeRateForCombine(string IsoWorkingPoint)
 	 IsoWorkingPoint == "VVTight");
   if(IsoWorkingPoint == "VLoose")
     {	
-      JetDistribution = (TH1F*) FakeRateDeterminedDistributions->Get("VLoosePTFRJetDistribution");
-      JetDistribution_DOWN = (TH1F*) FakeRateDeterminedDistributions->Get("VLooseLowJetDistribution");
-      JetDistribution_UP = (TH1F*) FakeRateDeterminedDistributions->Get("VLooseHighJetDistribution");
+      JetDistribution = (TH1F*) FakeRateDeterminedDistributions->Get("VLoosePTFRJetDistribution");      
+      JetDistribution_DecayMode0_DOWN = (TH1F*) FakeRateDeterminedDistributions->Get("VLooseDecayMode0LowJetDistribution");
+      JetDistribution_DecayMode0_UP = (TH1F*) FakeRateDeterminedDistributions->Get("VLooseDecayMode0HighJetDistribution");
+      JetDistribution_DecayMode1_DOWN = (TH1F*) FakeRateDeterminedDistributions->Get("VLooseDecayMode1LowJetDistribution");
+      JetDistribution_DecayMode1_UP = (TH1F*) FakeRateDeterminedDistributions->Get("VLooseDecayMode1HighJetDistribution");
+      JetDistribution_DecayMode10_DOWN = (TH1F*) FakeRateDeterminedDistributions->Get("VLooseDecayMode1LowJetDistribution");
+      JetDistribution_DecayMode10_UP = (TH1F*) FakeRateDeterminedDistributions->Get("VLooseDecayMode1HighJetDistribution");
     }
   else if(IsoWorkingPoint == "Loose")
     {
       JetDistribution = (TH1F*) FakeRateDeterminedDistributions->Get("LoosePTFRJetDistribution");
-      JetDistribution_DOWN = (TH1F*) FakeRateDeterminedDistributions->Get("LooseLowJetDistribution");
-      JetDistribution_UP = (TH1F*) FakeRateDeterminedDistributions->Get("LooseHighJetDistribution");
+      JetDistribution_DecayMode0_DOWN = (TH1F*) FakeRateDeterminedDistributions->Get("LooseDecayMode0LowJetDistribution");
+      JetDistribution_DecayMode0_UP = (TH1F*) FakeRateDeterminedDistributions->Get("LooseDecayMode0HighJetDistribution");
+      JetDistribution_DecayMode1_DOWN = (TH1F*) FakeRateDeterminedDistributions->Get("LooseDecayMode1LowJetDistribution");
+      JetDistribution_DecayMode1_UP = (TH1F*) FakeRateDeterminedDistributions->Get("LooseDecayMode1HighJetDistribution");
+      JetDistribution_DecayMode10_DOWN = (TH1F*) FakeRateDeterminedDistributions->Get("LooseDecayMode1LowJetDistribution");
+      JetDistribution_DecayMode10_UP = (TH1F*) FakeRateDeterminedDistributions->Get("LooseDecayMode1HighJetDistribution");
     }
   else if(IsoWorkingPoint == "Medium")
     {
-      JetDistribution = (TH1F*) FakeRateDeterminedDistributions->Get("MediumPTFRJetDistribution");
-      JetDistribution_DOWN = (TH1F*) FakeRateDeterminedDistributions->Get("MediumLowJetDistribution");
-      JetDistribution_UP = (TH1F*) FakeRateDeterminedDistributions->Get("MediumHighJetDistribution");
+      JetDistribution = (TH1F*) FakeRateDeterminedDistributions->Get("MediumPTFRJetDistribution");      
+      JetDistribution_DecayMode0_DOWN = (TH1F*) FakeRateDeterminedDistributions->Get("MediumDecayMode0LowJetDistribution");
+      JetDistribution_DecayMode0_UP = (TH1F*) FakeRateDeterminedDistributions->Get("MediumDecayMode0HighJetDistribution");
+      JetDistribution_DecayMode1_DOWN = (TH1F*) FakeRateDeterminedDistributions->Get("MediumDecayMode1LowJetDistribution");
+      JetDistribution_DecayMode1_UP = (TH1F*) FakeRateDeterminedDistributions->Get("MediumDecayMode1HighJetDistribution");
+      JetDistribution_DecayMode10_DOWN = (TH1F*) FakeRateDeterminedDistributions->Get("MediumDecayMode1LowJetDistribution");
+      JetDistribution_DecayMode10_UP = (TH1F*) FakeRateDeterminedDistributions->Get("MediumDecayMode1HighJetDistribution");
     }
   else if(IsoWorkingPoint == "Tight")
     {
-      JetDistribution = (TH1F*) FakeRateDeterminedDistributions->Get("TightPTFRJetDistribution");
-      JetDistribution_DOWN = (TH1F*) FakeRateDeterminedDistributions->Get("TightLowJetDistribution");
-      JetDistribution_UP = (TH1F*) FakeRateDeterminedDistributions->Get("TightHighJetDistribution");
+      JetDistribution = (TH1F*) FakeRateDeterminedDistributions->Get("TightPTFRJetDistribution");      
+      JetDistribution_DecayMode0_DOWN = (TH1F*) FakeRateDeterminedDistributions->Get("TightDecayMode0LowJetDistribution");
+      JetDistribution_DecayMode0_UP = (TH1F*) FakeRateDeterminedDistributions->Get("TightDecayMode0HighJetDistribution");
+      JetDistribution_DecayMode1_DOWN = (TH1F*) FakeRateDeterminedDistributions->Get("TightDecayMode1LowJetDistribution");
+      JetDistribution_DecayMode1_UP = (TH1F*) FakeRateDeterminedDistributions->Get("TightDecayMode1HighJetDistribution");
+      JetDistribution_DecayMode10_DOWN = (TH1F*) FakeRateDeterminedDistributions->Get("TightDecayMode1LowJetDistribution");
+      JetDistribution_DecayMode10_UP = (TH1F*) FakeRateDeterminedDistributions->Get("TightDecayMode1HighJetDistribution");
     }
   else if(IsoWorkingPoint == "VTight")
     {
-      JetDistribution = (TH1F*) FakeRateDeterminedDistributions->Get("VTightPTFRJetDistribution");
-      JetDistribution_DOWN = (TH1F*) FakeRateDeterminedDistributions->Get("VTightLowJetDistribution");
-      JetDistribution_UP = (TH1F*) FakeRateDeterminedDistributions->Get("VTightHighJetDistribution");
+      JetDistribution = (TH1F*) FakeRateDeterminedDistributions->Get("VTightPTFRJetDistribution");      
+      JetDistribution_DecayMode0_DOWN = (TH1F*) FakeRateDeterminedDistributions->Get("VTightDecayMode0LowJetDistribution");
+      JetDistribution_DecayMode0_UP = (TH1F*) FakeRateDeterminedDistributions->Get("VTightDecayMode0HighJetDistribution");
+      JetDistribution_DecayMode1_DOWN = (TH1F*) FakeRateDeterminedDistributions->Get("VTightDecayMode1LowJetDistribution");
+      JetDistribution_DecayMode1_UP = (TH1F*) FakeRateDeterminedDistributions->Get("VTightDecayMode1HighJetDistribution");
+      JetDistribution_DecayMode10_DOWN = (TH1F*) FakeRateDeterminedDistributions->Get("VTightDecayMode1LowJetDistribution");
+      JetDistribution_DecayMode10_UP = (TH1F*) FakeRateDeterminedDistributions->Get("VTightDecayMode1HighJetDistribution");
     }
   else if(IsoWorkingPoint == "VVTight")
     {
-      JetDistribution = (TH1F*) FakeRateDeterminedDistributions->Get("VVTightPTFRJetDistribution");
-      JetDistribution_DOWN = (TH1F*) FakeRateDeterminedDistributions->Get("VVTightLowJetDistribution");
-      JetDistribution_UP = (TH1F*) FakeRateDeterminedDistributions->Get("VVTightHighJetDistribution");
-    }
-  TH1F* Jets_WQCDShape_DOWN = (TH1F*) FakeRateDeterminedDistributions->Get("Jets_WQCDShapeDown");
-  TH1F* Jets_WQCDShape_UP = (TH1F*) FakeRateDeterminedDistributions->Get("Jets_WQCDShapeUp");
+      JetDistribution = (TH1F*) FakeRateDeterminedDistributions->Get("VVTightPTFRJetDistribution");      
+      JetDistribution_DecayMode0_DOWN = (TH1F*) FakeRateDeterminedDistributions->Get("VVTightDecayMode0LowJetDistribution");
+      JetDistribution_DecayMode0_UP = (TH1F*) FakeRateDeterminedDistributions->Get("VVTightDecayMode0HighJetDistribution");
+      JetDistribution_DecayMode1_DOWN = (TH1F*) FakeRateDeterminedDistributions->Get("VVTightDecayMode1LowJetDistribution");
+      JetDistribution_DecayMode1_UP = (TH1F*) FakeRateDeterminedDistributions->Get("VVTightDecayMode1HighJetDistribution");
+      JetDistribution_DecayMode10_DOWN = (TH1F*) FakeRateDeterminedDistributions->Get("VVTightDecayMode1LowJetDistribution");
+      JetDistribution_DecayMode10_UP = (TH1F*) FakeRateDeterminedDistributions->Get("VVTightDecayMode1HighJetDistribution");
+    }    
 
   std::cout<<"Simplifying the DY Histos"<<std::endl;
   //pass compiling  
@@ -98,6 +158,15 @@ void PrepareFakeRateForCombine(string IsoWorkingPoint)
   ZTauTau_Pass->Add(ZTauTau2_Pass);
   ZTauTau_Pass->Add(ZTauTau3_Pass);
   ZTauTau_Pass->Add(ZTauTau4_Pass);
+
+  ZTauTau_TES_UP_Pass->Add(ZTauTau1_TES_UP_Pass);
+  ZTauTau_TES_UP_Pass->Add(ZTauTau2_TES_UP_Pass);
+  ZTauTau_TES_UP_Pass->Add(ZTauTau3_TES_UP_Pass);
+  ZTauTau_TES_UP_Pass->Add(ZTauTau4_TES_UP_Pass);
+  ZTauTau_TES_DOWN_Pass->Add(ZTauTau1_TES_DOWN_Pass);
+  ZTauTau_TES_DOWN_Pass->Add(ZTauTau2_TES_DOWN_Pass);
+  ZTauTau_TES_DOWN_Pass->Add(ZTauTau3_TES_DOWN_Pass);
+  ZTauTau_TES_DOWN_Pass->Add(ZTauTau4_TES_DOWN_Pass);
 
   std::cout<<"Loading the ZMM Region..."<<std::endl;
   TFile* ZMuMuFile = new TFile("../Distributions/ZMuMu.root");
@@ -169,6 +238,12 @@ void PrepareFakeRateForCombine(string IsoWorkingPoint)
   LowGenMatch_DY_Pass->SetNameTitle("DYB","DYB");
   LowGenMatch_DY_Pass->Write();
 
+  LowGenMatch_DY_Shape_UP_Pass->SetNameTitle("DYB_muToTauUp","DYB_muToTauUp");
+  LowGenMatch_DY_Shape_UP_Pass->Write();
+
+  LowGenMatch_DY_Shape_DOWN_Pass->SetNameTitle("DYB_muToTauDown","DYB_muToTauDown");
+  LowGenMatch_DY_Shape_DOWN_Pass->Write();
+
   std::cout<<"DYJ..."<<std::endl;
 
   HighGenMatch_DY_Pass->SetNameTitle("DYJ","DYJ");
@@ -178,6 +253,12 @@ void PrepareFakeRateForCombine(string IsoWorkingPoint)
 
   ZTauTau_Pass->SetNameTitle("DYS","DYS");  
   ZTauTau_Pass->Write();
+
+  ZTauTau_TES_UP_Pass->SetNameTitle("DYS_TESUp","DYS_TESUp");
+  ZTauTau_TES_UP_Pass->Write();
+
+  ZTauTau_TES_DOWN_Pass->SetNameTitle("DYS_TESDown","DYS_TESDown");
+  ZTauTau_TES_DOWN_Pass->Write();
 
   std::cout<<"TTbar..."<<std::endl;
 
@@ -206,14 +287,23 @@ void PrepareFakeRateForCombine(string IsoWorkingPoint)
   JetDistribution->SetNameTitle("Jets","Jets");
   JetDistribution->Write();
 
-  JetDistribution_DOWN->SetNameTitle("Jets_FakeRateUncertaintyDown","Jets_FakeRateUncertaintyDown");
-  JetDistribution_DOWN->Write();
-  
-  JetDistribution_UP->SetNameTitle("Jets_FakeRateUncertaintyUp","Jets_FakeRateUncertaintyUp");
-  JetDistribution_UP->Write();
+  JetDistribution_DecayMode0_DOWN->SetNameTitle("Jets_DM0FakeRateUncertaintyDown","Jets_DM0FakeRateUncertaintyDown");
+  JetDistribution_DecayMode0_DOWN->Write();
 
-  //Jets_WQCDShape_DOWN->Write();
-  //Jets_WQCDShape_UP->Write();
+  JetDistribution_DecayMode0_UP->SetNameTitle("Jets_DM0FakeRateUncertaintyUp","Jets_DM0FakeRateUncertaintyUp");
+  JetDistribution_DecayMode0_UP->Write();
+
+  JetDistribution_DecayMode1_DOWN->SetNameTitle("Jets_DM1FakeRateUncertaintyDown","Jets_DM1FakeRateUncertaintyDown");
+  JetDistribution_DecayMode1_DOWN->Write();
+
+  JetDistribution_DecayMode1_UP->SetNameTitle("Jets_DM1FakeRateUncertaintyUp","Jets_DM1FakeRateUncertaintyUp");
+  JetDistribution_DecayMode1_UP->Write();
+
+  JetDistribution_DecayMode10_DOWN->SetNameTitle("Jets_DM10FakeRateUncertaintyDown","Jets_DM10FakeRateUncertaintyDown");
+  JetDistribution_DecayMode10_DOWN->Write();
+
+  JetDistribution_DecayMode10_UP->SetNameTitle("Jets_DM10FakeRateUncertaintyUp","Jets_DM10FakeRateUncertaintyUp");
+  JetDistribution_DecayMode10_UP->Write();
   
   std::cout<<"Preparing the Zmm region"<<std::endl;  
   TDirectory* ZMMDirectory = CombineFile->mkdir("zmm");
@@ -253,34 +343,6 @@ void PrepareFakeRateForCombine(string IsoWorkingPoint)
 			     DY_MuMu->GetXaxis()->GetXmin(),
 			     DY_MuMu->GetXaxis()->GetXmax());
   Jets_MuMu->Write();
-
-  TH1F* Jets_MuMu_DOWN = new TH1F("Jets_FakeRateUncertaintyDown",
-				  "Jets_FakeRateUncertaintyDown",
-				  DY_MuMu->GetSize()-2,
-				  DY_MuMu->GetXaxis()->GetXmin(),
-				  DY_MuMu->GetXaxis()->GetXmax());
-  Jets_MuMu_DOWN->Write();
-
-  TH1F* Jets_MuMu_UP = new TH1F("Jets_FakeRateUncertaintyUp",
-				"Jets_FakeRateUncertaintyUp",
-				DY_MuMu->GetSize()-2,
-				DY_MuMu->GetXaxis()->GetXmin(),
-				DY_MuMu->GetXaxis()->GetXmax());
-  Jets_MuMu_UP->Write();
-
-  TH1F* Jets_MuMu_WQCDShape_DOWN = new TH1F("Jets_WQCDShapeDown",
-					    "Jets_WQCDShapeDown",
-					    DY_MuMu->GetSize()-2,
-					    DY_MuMu->GetXaxis()->GetXmin(),
-					    DY_MuMu->GetXaxis()->GetXmax());
-  Jets_MuMu_WQCDShape_DOWN->Write();
-
-  TH1F* Jets_MuMu_WQCDShape_UP = new TH1F("Jets_WQCDShapeUp",
-					  "Jets_WQCDShapeUp",
-					  DY_MuMu->GetSize()-2,
-					  DY_MuMu->GetXaxis()->GetXmin(),
-					  DY_MuMu->GetXaxis()->GetXmax());
-  Jets_MuMu_WQCDShape_UP->Write();
 
   TH1F* DYS_MuMu = new TH1F("DYS",
 			    "DYS",
